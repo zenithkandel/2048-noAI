0358a0b86acd35bd094a7f4beea6ba05ad6743d9
Zenith Kandel <kandelze123@gmail.com>
2026-05-04 20:12:02 +0545

    Refactor game array initialization and rendering logic; enhance grid styling for better layout

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ let grid_size_chips = document.querySelectorAll(".grid-size");
 let control_restart_chip = document.querySelector(".control-restart");
 let control_pause_chip = document.querySelector(".control-pause");
 let control_save_chip = document.querySelector(".control-save");
-
+let game_arr = [];
 if (!localStorage.getItem('game_arr')) {
-    let game_arr = [];
+    game_arr = [];
 }
 else {
-    let game_arr = localStorage.getItem("game_arr");
+    game_arr = localStorage.getItem("game_arr");
 }
 
 let game_size = "4";
@@ -24,3 +24,36 @@ grid_size_chips.forEach(thing => {
         console.log(game_size);
     }
 });
+
+function initializeGrid() {
+    game_arr = [];
+    for (let i = 0; i < game_size; i++) {
+        tmp = [];
+        for (let j = 0; j < game_size; j++) {
+            tmp.push("0")
+        }
+        game_arr.push(tmp);
+    }
+    console.log(JSON.stringify(game_arr))
+}
+
+function renderGrid() {
+    tmp = "";
+    tmp = "<table>";
+    for (let i = 0; i < game_size; i++) {
+        // tmp = [];
+        tmp += "<tr>"
+        for (let j = 0; j < game_size; j++) {
+            // tmp.push("0");
+            tmp += "<td>" + game_arr[i][j] + "</td>";
+        }
+        // game_arr.push(tmp);
+        tmp += "</tr>"
+    }
+    // console.log(JSON.stringify(game_arr))
+    tmp += "</table>";
+    grid_element.innerHTML = tmp;
+}
+
+initializeGrid()
+renderGrid()
