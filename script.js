87c27922fca9fb92f517cef7486eafe22bf4a8d8
Zenith Kandel <kandelze123@gmail.com>
2026-05-04 19:33:15 +0545

    Add control chips for restart, pause, and save functionality

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 let grid_element = document.querySelector(".grid");
 let grid_sizes = ["4x4", "8x8", "16x16"];
 let grid_size_chips = document.querySelectorAll(".grid-size");
+let control_restart_chip = document.querySelector(".control-restart");
+let control_pause_chip = document.querySelector(".control-pause");
+let control_save_chip = document.querySelector(".control-save");
+
